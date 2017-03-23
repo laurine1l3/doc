a0905416bdfe8c1de1e17d007acdf1beb40bfeaf
laurine1l3 <laurinecheng@gmail.com>
2017-03-22 22:39:04 -0700

    BankAccount - main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 //  main.cpp
 //  Lab5
 //
-//  Created by Cheng, Laurine(AWF) on 3/5/17.
-//  Copyright © 2017 Cheng, Laurine(AWF). All rights reserved.
-//
-
 #include <iostream>
 #include "Checking.h"
 #include "Savings.h"
